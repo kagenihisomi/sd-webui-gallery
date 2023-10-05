678fb5cdfbe3bfe542948ec7865fe4dcd3adb18f
kagenihisomi <asdftoger@gmail.com>
2023-10-05 20:13:39 +1100

    Remove tab

--- scripts/gallery.py
+++ scripts/gallery.py
@@ -19,70 +19,69 @@ DROPDOWN_SELECT = partial(
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as gallery:
-        with gr.Tabs(elem_id="image_gallery") as tabs:
-            # UI Components
-            df_image_infos = calc_df_image_infos()
-            prompts = calc_unique_options(df_image_infos, "prompt")
-            models = calc_unique_options(df_image_infos, "model")
-            dates = calc_unique_options(df_image_infos, "date")
-            sub_folder = calc_unique_options(df_image_infos, "sub_folder")
-            grdf_image_info = gr.DataFrame(df_image_infos, visible=False)
-            grdf_image_info_filtered = gr.DataFrame(df_image_infos, visible=False)
+        # UI Components
+        df_image_infos = calc_df_image_infos()
+        prompts = calc_unique_options(df_image_infos, "prompt")
+        models = calc_unique_options(df_image_infos, "model")
+        dates = calc_unique_options(df_image_infos, "date")
+        sub_folder = calc_unique_options(df_image_infos, "sub_folder")
+        grdf_image_info = gr.DataFrame(df_image_infos, visible=False)
+        grdf_image_info_filtered = gr.DataFrame(df_image_infos, visible=False)
 
-            with gr.Row():
-                with gr.Column():
-                    image_gallery = gr.Gallery(
-                        value=df_image_infos["path_full"].tolist(),
-                        label="Select an image to view information",
-                        show_label=True,
-                        columns=3,
-                        preview=False,
-                    )
-
-                with gr.Column():
-                    gr.Textbox("Search and filter images", show_label=False)
-
-                    sub_folder_dropdown = DROPDOWN_SELECT(
-                        value=[],
-                        choices=sub_folder,
-                        label="Folder",
-                    )
-                    date_dropdown = DROPDOWN_SELECT(
-                        value=[],
-                        choices=dates,
-                        label="Date",
-                    )
-                    model_dropdown = DROPDOWN_SELECT(
-                        value=[],
-                        choices=models,
-                        label="Model",
-                    )
-                    prompts_dropdown = DROPDOWN_SELECT(
-                        value=[],
-                        choices=prompts,
-                        label="Prompts",
-                    )
-
-                    textbox_image_info = gr.Textbox("", show_label=False)
-
-            # Drop UI Callbacks
-            dropdowns = [
-                sub_folder_dropdown,
-                date_dropdown,
-                model_dropdown,
-                prompts_dropdown,
-            ]
-
-            for dropdown in dropdowns:
-                dropdown.select(
-                    filter_df_image_infos,
-                    [grdf_image_info, *dropdowns],
-                    [image_gallery, grdf_image_info_filtered, *dropdowns],
+        with gr.Row():
+            with gr.Column():
+                image_gallery = gr.Gallery(
+                    value=df_image_infos["path_full"].tolist(),
+                    label="Select an image to view information",
+                    show_label=True,
+                    columns=3,
+                    preview=False,
                 )
 
-            image_gallery.select(
-                display_image_info, [grdf_image_info_filtered], textbox_image_info
+            with gr.Column():
+                gr.Textbox("Search and filter images", show_label=False)
+
+                sub_folder_dropdown = DROPDOWN_SELECT(
+                    value=[],
+                    choices=sub_folder,
+                    label="Folder",
+                )
+                date_dropdown = DROPDOWN_SELECT(
+                    value=[],
+                    choices=dates,
+                    label="Date",
+                )
+                model_dropdown = DROPDOWN_SELECT(
+                    value=[],
+                    choices=models,
+                    label="Model",
+                )
+                prompts_dropdown = DROPDOWN_SELECT(
+                    value=[],
+                    choices=prompts,
+                    label="Prompts",
+                )
+
+                textbox_image_info = gr.Textbox("", show_label=False)
+
+        # Drop UI Callbacks
+        dropdowns = [
+            sub_folder_dropdown,
+            date_dropdown,
+            model_dropdown,
+            prompts_dropdown,
+        ]
+
+        for dropdown in dropdowns:
+            dropdown.select(
+                filter_df_image_infos,
+                [grdf_image_info, *dropdowns],
+                [image_gallery, grdf_image_info_filtered, *dropdowns],
             )
+
+        image_gallery.select(
+            display_image_info, [grdf_image_info_filtered], textbox_image_info
+        )
 
     return ((gallery, "Image Gallery", "gallery"),)
 
